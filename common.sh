91358ce575ba4ba126501c7eeba420a5c10e3775
Radhika <radhika.b4329@gmail.com>
2023-01-31 21:37:02 +0530

    script changes

--- common.sh
+++ common.sh
@@ -3,10 +3,14 @@ LOG=/tmp/roboshop.log
 
 status_check() {
   if [ $? -eq 0 ]; then
-    echo -e "\e[32mSUCCESS\e[0m"
+    echo -e "\e[1;32mSUCCESS\e[0m"
   else
     echo -e "\e[31mFAILURE\e[0m"
     echo "Refer Log file for more information, LOG - ${LOG}"
     exit
     fi
+}
+
+print_head
+ echo -e "\e[1m $1 \e[0m"
 }
