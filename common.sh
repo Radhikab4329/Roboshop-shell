71bd42cd2f5a6825d8075af4a6d2f7bc3aab7a6c
Radhika <radhika.b4329@gmail.com>
2023-02-12 22:19:55 +0530

    mysql

--- common.sh
+++ common.sh
@@ -15,23 +15,16 @@ print_head() {
  echo -e "\e[1m $1 \e[0m"
 }
 
-NODEJS() {
-print_head "Configuring NodeJS Repos"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
-status_check
+APP_PREREQ() {
 
-print_head "Install NodeJS"
-yum install nodejs -y &>>${LOG}
-status_check
+ print_head "Add Application User"
+ id roboshop &>>${LOG}
+ if [ $? -ne 0 ]; then
+   useradd roboshop &>>${LOG}
+ fi
+ status_check
 
-print_head "Add Application User"
-id roboshop &>>${LOG}
-if [ $? -ne 0 ]; then
-  useradd roboshop &>>${LOG}
-fi
-status_check
-
-mkdir -p /app &>>${LOG}
+ mkdir -p /app &>>${LOG}
 
 print_head "Downloading App content"
 curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${LOG}
@@ -46,38 +39,96 @@ cd /app
 unzip /tmp/${component}.zip &>>${LOG}
 status_check
 
+}
+
+SYSTEMD_SETUP() {
+  print_head "Configuring ${component} Service File"
+  cp ${script_location}/files/${component}.service /etc/systemd/system/${component}.service &>>${LOG}
+  status_check
+
+  print_head "Reload systemD"
+  systemctl daemon-reload &>>${LOG}
+  status_check
+
+  print_head "Enable ${component} Service"
+  systemctl enable ${component} &>>${LOG}
+  status_check
+
+  print_head "Start ${component} Service"
+  systemctl start ${component} &>>${LOG}
+  status_check
+}
+
+LOAD_SCHEMA() {
+  if [ ${schema_load} == "true" ]; then
+
+    if [ ${schema_type} == "mongo" ]; then
+      print_head "Configuring Mongo Repo "
+      cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${LOG}
+      status_check
+
+      print_head "Install Mongo Client"
+      yum install mongodb-org-shell -y &>>${LOG}
+      status_check
+
+      print_head "Load Schema"
+      mongo --host mongodb-dev.learndevops43.online </app/schema/${component}.js &>>${LOG}
+      status_check
+      fi
+
+      if [ ${schema_type} == "mysql" ]; then
+
+            print_head "Install MYSQL Client"
+            yum install mysql -y &>>${LOG}
+            status_check
+
+            print_head "Load Schema"
+            mongo mysql -h mysql-dev.learndevops43.online -uroot -p${root_mysql_password} < /app/schema/shipping.sql  &>>${LOG}
+            status_check
+            fi
+
+    fi
+}
+
+NODEJS() {
+print_head "Configuring NodeJS Repos"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
+status_check
+
+print_head "Install NodeJS"
+yum install nodejs -y &>>${LOG}
+status_check
+
+APP_PREREQ
+
 print_head "Installing NodeJS Dependencies"
 cd /app &>>${LOG}
 npm install &>>${LOG}
 status_check
 
-print_head "Configuring ${component} Service File"
-cp ${script_location}/files/${component}.service /etc/systemd/system/${component}.service &>>${LOG}
-status_check
+ SYSTEMD_SETUP
 
-print_head "Reload systemD"
-systemctl daemon-reload &>>${LOG}
-status_check
+LOAD_SCHEMA
+}
 
-print_head "Enable ${component} Service"
-systemctl enable ${component} &>>${LOG}
-status_check
+MAVEN() {
 
-print_head "Start ${component} Service"
-systemctl start ${component} &>>${LOG}
-status_check
+  print_head "Install Maven"
+  yum install maven -y &>>${LOG}
+  status_check
 
-if [ ${schema_load} == "true" ]; then
-    print_head "Configuring Mongo Repo "
-    cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${LOG}
-    status_check
+  APP_PREREQ
 
-    print_head "Install Mongo Client"
-    yum install mongodb-org-shell -y &>>${LOG}
-    status_check
+  print_head "Build a package"
+  mvn clean package  &>>${LOG}
+  status_check
 
-    print_head "Load Schema"
-    mongo --host mongodb-dev.devopsb70.online </app/schema/${component}.js &>>${LOG}
-    status_check
-  fi
+  print_head "Copy App file to App Location"
+  mv target/${component}-1.0.jar ${component}.jar
+  status_check
+
+  SYSTEMD_SETUP
+
+  LOAD_SCHEMA
+
 }
