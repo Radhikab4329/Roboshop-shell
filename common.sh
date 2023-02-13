c3e7153e180623366d70785035e2a8da34017b3c
Radhika <radhika.b4329@gmail.com>
2023-02-13 15:05:45 +0530

    shipping

--- common.sh
+++ common.sh
@@ -15,48 +15,54 @@ print_head() {
  echo -e "\e[1m $1 \e[0m"
 }
 
-NODEJS() {
-  print_head "Configuring NodeJS Repos"
-  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
-  status_check
-
-  print_head "Install NodeJS"
-  yum install nodejs -y &>>${LOG}
-  status_check
 APP_PREREQ() {
 
-  print_head "Add Application User"
-  id roboshop &>>${LOG}
-NODEJS() {
-  unzip /tmp/${component}.zip &>>${LOG}
-  status_check
+ print_head "Add Application User"
+ id roboshop &>>${LOG}
+ if [ $? -ne 0 ]; then
+   useradd roboshop &>>${LOG}
+ fi
+ status_check
 
-  print_head "Installing NodeJS Dependencies"
-  cd /app &>>${LOG}
-  npm install &>>${LOG}
-  status_check
+ mkdir -p /app &>>${LOG}
+
+print_head "Downloading App content"
+curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${LOG}
+status_check
+
+print_head "Cleanup Old Content"
+rm -rf /app/* &>>${LOG}
+status_check
+
+print_head "Extracting App Content"
+cd /app
+unzip /tmp/${component}.zip &>>${LOG}
+status_check
+
 }
 
 SYSTEMD_SETUP() {
   print_head "Configuring ${component} Service File"
   cp ${script_location}/files/${component}.service /etc/systemd/system/${component}.service &>>${LOG}
   status_check
-NODEJS() {
-  print_head "Start ${component} service "
+
+  print_head "Reload systemD"
+  systemctl daemon-reload &>>${LOG}
+  status_check
+
+  print_head "Enable ${component} Service"
+  systemctl enable ${component} &>>${LOG}
+  status_check
+
+  print_head "Start ${component} Service"
   systemctl start ${component} &>>${LOG}
   status_check
 }
 
 LOAD_SCHEMA() {
   if [ ${schema_load} == "true" ]; then
-    print_head "Configuring Mongo Repo "
-    cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${LOG}
-    status_check
 
-    print_head "Install Mongo Client"
-    yum install mongodb-org-shell -y &>>${LOG}
-    status_check
-    if [ ${schema_type} == "mongo"  ]; then
+    if [ ${schema_type} == "mongo" ]; then
       print_head "Configuring Mongo Repo "
       cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${LOG}
       status_check
@@ -66,46 +72,43 @@ LOAD_SCHEMA() {
       status_check
 
       print_head "Load Schema"
-      mongo --host mongodb-dev.devopsb70.online </app/schema/${component}.js &>>${LOG}
+      mongo --host mongodb-dev.learndevops43.online </app/schema/${component}.js &>>${LOG}
       status_check
+      fi
+
+      if [ ${schema_type} == "mysql" ]; then
+
+            print_head "Install MYSQL Client"
+            yum install mysql -y &>>${LOG}
+            status_check
+
+            print_head "Load Schema"
+            mysql -h mysql-dev.learndevops43.online -uroot -p${root_mysql_password} < /app/schema/shipping.sql  &>>${LOG}
+            status_check
+            fi
+
     fi
-
-    if [ ${schema_type} == "mysql"  ]; then
-
-      print_head "Install MySQL Client"
-      yum install mysql -y &>>${LOG}
-      status_check
-
-      print_head "Load Schema"
-      mysql -h mysql-dev.devopsb70.online -uroot -p${root_mysql_password} < /app/schema/shipping.sql  &>>${LOG}
-      status_check
-    fi
-
-    print_head "Load Schema"
-    mongo --host mongodb-dev.devopsb70.online </app/schema/${component}.js &>>${LOG}
-    status_check
-  fi
 }
 
 NODEJS() {
-  print_head "Configuring NodeJS Repos"
-  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
-  status_check
+print_head "Configuring NodeJS Repos"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
+status_check
 
-  print_head "Install NodeJS"
-  yum install nodejs -y &>>${LOG}
-  status_check
+print_head "Install NodeJS"
+yum install nodejs -y &>>${LOG}
+status_check
 
-  APP_PREREQ
+APP_PREREQ
 
-  print_head "Installing NodeJS Dependencies"
-  cd /app &>>${LOG}
-  npm install &>>${LOG}
-  status_check
+print_head "Installing NodeJS Dependencies"
+cd /app &>>${LOG}
+npm install &>>${LOG}
+status_check
 
-  SYSTEMD_SETUP
+SYSTEMD_SETUP
 
-  LOAD_SCHEMA
+LOAD_SCHEMA
 }
 
 MAVEN() {
